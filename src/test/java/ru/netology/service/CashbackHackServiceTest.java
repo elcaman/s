bf95664cc168f806035b8f0e625c30ae61c7046c
nberez <nikol0880@outlook.com>
2024-08-06 16:20:31 +0300

    adding test class

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -30,6 +30,16 @@ public class CashbackHackServiceTest {
     }
 
     @Test
+    public void purchaseAmount1000(){
+        CashbackHackService CashService = new CashbackHackService();
+        int amount = 1000;
+        int actual = CashService.remain(amount);
+        int expected = 0;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
     public void purchaseAmount1500(){
         CashbackHackService CashService = new CashbackHackService();
         int amount = 1500;
